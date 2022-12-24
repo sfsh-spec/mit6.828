61ea08b039d74d3a2f19223dc74a1a96f1ae7f6a
biu <420043343@qq.com>
2022-12-24 16:17:08 +0800

    update lab2

--- kern/init.c
+++ kern/init.c
@@ -35,8 +35,8 @@ i386_init(void)
 
 	cprintf("6828 decimal is %o octal!\n", 6828);
 
-	// Test the stack backtrace function (lab 1 only)
-	test_backtrace(5);
+	// Lab 2 memory management initialization functions
+	mem_init();
 
 	// Drop into the kernel monitor.
 	while (1)
